693cddbd2f9f79bc14f7cae030d20f5549283930
RyonosukeKondo <ryonosuke.kondo@sharing-innovations.co.jp>
2024-08-23 11:25:23 +0900

    未読お知らせフィルターとクリック時処理を修正・追加_240823

--- background.js
+++ background.js
@@ -1,16 +1,51 @@
 // Backlog API の設定
 const BACKLOG_API_URL = 'https://ayuta.backlog.jp/api/v2';
-const BACKLOG_API_KEY = 'L4hgXavObdHHaVHMdtKxerYV0nbASWwKhc7Y8tacj5jPPQEzuBOt6tjPSgjj80JN';
+const BACKLOG_API_KEY = 'JXqlSGW1lXAePstoikTnWSWa6vV7AWZvMJZ3Kr7zjvIaix2JbO6SFmDnIY0DaETr';
 
 // 通知設定
 const NOTIFICATION_TITLE = 'Backlog: お知らせ';
 const NOTIFICATION_ICON = 'icons/task_alert48.png';
+const DASHBOARD_URL = 'https://ayuta.backlog.jp/dashboard';
 
 // 通知する間隔
-const CHECK_INTERVAL = 15;
+const CHECK_INTERVAL = 5;
 
 // スケジュールの設定
 chrome.alarms.create('checkNotifications', { periodInMinutes: CHECK_INTERVAL });
+
+// アラームがトリガーされたときに実行される処理
+chrome.alarms.onAlarm.addListener(alarm => {
+  if (alarm.name === 'checkNotifications') {
+    fetchNotifications();
+  }
+});
+
+fetchNotifications();
+
+// 通知がクリックされたときの処理
+chrome.notifications.onClicked.addListener(notificationId => {
+  console.log('クリックされた通知のID:', notificationId);
+  // 通知がクリックされたときに遷移するURLを指定
+  chrome.tabs.create({ url: DASHBOARD_URL });
+});
+
+// Backlog API から通知数を取得する関数
+async function fetchNotifications() {
+  try {
+    const response = await fetch(`${BACKLOG_API_URL}/notifications?apiKey=${BACKLOG_API_KEY}&isRead=false`);
+    if (!response.ok) {
+      throw new Error('ネットワークレスポンスにエラーが発生しました');
+    }
+    const unreadNotifications = await response.json();
+
+    // 未読通知が存在する場合
+    if (unreadNotifications.length > 0) {
+      showNotification(`未読のお知らせが ${unreadNotifications.length} 件取得されました。`);
+    }
+  } catch (error) {
+    console.error('未読のお知らせ取得に失敗しました', error);
+  }
+}
 
 // 通知を表示する関数
 function showNotification(message) {
@@ -22,36 +57,3 @@ function showNotification(message) {
     priority: 2
   });
 }
-
-// Backlog API から通知数を取得する関数
-async function fetchNotifications() {
-  try {
-    const response = await fetch(`${BACKLOG_API_URL}/notifications?apiKey=${BACKLOG_API_KEY}`);
-    if (!response.ok) {
-      throw new Error('ネットワークレスポンスにエラーが発生しました');
-    }
-    const notifications = await response.json();
-
-    // お知らせの数をチェック
-    const unreadNotifications = notifications.filter(notification => !notification.isRead);
-
-    // 未読通知が存在する場合
-    if (unreadNotifications > 0) {
-      showNotification(`新しいお知らせが ${unreadNotifications} 件来ています`);
-    } else{
-      showNotification(`新しいお知らせはありませんでした`);
-    }
-  } catch (error) {
-    console.error('お知らせの取得に失敗しました', error);
-  }
-}
-
-// アラームがトリガーされたときに実行される処理
-chrome.alarms.onAlarm.addListener(alarm => {
-  if (alarm.name === 'checkNotifications') {
-    fetchNotifications();
-  }
-});
-
-// 拡張機能が初期化されたときに最初のチェックを実行
-fetchNotifications();
